194e5a31bf65aa192e9da2930a4b0d41f868f78a
thealexandar <aleksandardmitrovic@yahoo.com>
2023-02-28 20:34:03 +0100

    added dashboard page and redirect after login

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-2 sidebar"></div>
+        <div class="col-lg-10 main">
+            <h1>Dashboard</h1>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
